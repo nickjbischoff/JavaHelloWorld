7595edaa31ba9e380c60ae74420b1637e1c35de9
unknown <nickjbischoff@gmail.com>
2021-04-14 23:20:21 -0400

    inital commit and push for HelloWorld app

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ package com.mycompany.helloworldproject;
 
 public class Main {
    public static void main(String [] args) {
-       System.out.println("Hello World ! ");
+       System.out.println("Hello World ! :) ");
        
    } 
 }
