c3faa6775a6d7177c8b6980d8f102e4f4ff993f2
unknown <nickjbischoff@gmail.com>
2021-04-14 23:16:09 -0400

    init and first commit/push for HelloWorld app

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.mycompany.helloworldproject;
+/**
+ * @author nick-
+ */
+
+public class Main {
+   public static void main(String [] args) {
+       System.out.println("Hello World ! ");
+       
+   } 
+}
